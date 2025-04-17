02ba18f4dce09946a6be2731242d4f5758fe1a56
potato <naufalmaulanahasan@gmail.com>
2025-04-17 22:52:30 +0000

    penambahan fitur loading page, bug silang, popup

--- iot/app.py
+++ iot/app.py
@@ -196,4 +196,4 @@ def admin_unbook(booking_id):
 if __name__ == '__main__':
     if not os.path.exists('instance'):
         os.mkdir('instance')
-    app.run(debug=True, host='0.0.0.0', port=81)
+    app.run(debug=True, host='0.0.0.0', port=80)
